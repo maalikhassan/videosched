68b14dd896ebe5a3a2260c4bf1e58040bc203869
Maalik_Hassan <maalikhassan1@gmail.com>
2025-11-05 20:49:47 +0530

    reverted to prev online db and hid credintials , included assets upload

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection settings
-$host = 'sql308.infinityfree.com'; // Replace with the hostname provided by your hosting service
-$username = 'if0_40341082'; // Your database username
-$password = 'urB2d1jKPN3LE3Q'; // Your database password
-$dbname = 'if0_40341082_YTscheduler'; // Your database name
+$host = '************'; // Replace with the hostname provided by your hosting service Ex: 'sql308.infinityfree.com';
+$username = '**********'; // Your database username (Ex:'if0_40341***';)
+$password = '*************'; // Your database password (Ex:'urB2d1jKPN3****';)
+$dbname = '************'; // Your database name (Ex:'if0_4034*****_YTscheduler';)
 
 // Create a connection
 $conn = new mysqli($host, $username, $password, $dbname);
